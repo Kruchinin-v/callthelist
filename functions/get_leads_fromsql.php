eb2617d7b6100354f1d7be7442d27a8ffb362b1b
Kruchinin-v <vladislav97kr@gmail.com>
2020-09-14 00:12:16 +0300

    Вынос функции по поиску лидов в базе в отдельный файл

--- functions/get_leads_fromsql.php
+++ functions/get_leads_fromsql.php
@@ -1,1 +1,26 @@
 <?php
+/**
+ * Функция берет из базы лиды, по котором нужны звонки
+ * @return array: массив лидов
+ */
+function get_leads_fromsql() {
+    require_once 'connection.php'; // подключаем скрипт
+    # подключение к  базе amo
+    $link = mysqli_connect($host, $user, $password, $database_amo)
+    or die("Ошибка " . mysqli_error($link));
+
+    $query = "SELECT date, id_lead, count_call from leads";
+
+    $result = mysqli_query($link, $query) or die("Ошибка " . mysqli_error($link));
+
+    if(!$result) {
+        echo "<p>Выполнение запроса прошло не успешно</p>\n\n";
+    }
+
+    $data = [];
+
+    while ($row = $result->fetch_assoc()) {
+        $data[] = $row;
+    }
+    return $data;
+}
