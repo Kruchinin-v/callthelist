785ccc1062c14a3250351358b3e21fb42952b002
Kruchinin-v <vladislav97kr@gmail.com>
2020-09-14 00:11:17 +0300

    Вынос функции по поиску лидов в базе в отдельный файл

--- call.php
+++ call.php
@@ -12,6 +12,7 @@ include 'functions/getContact.php';
 include 'functions/getPhones.php';
 include 'functions/asteriskModule.php';
 include 'functions/environment.php';
+include 'functions/get_leads_fromsql.php';
 
 require('../tokens/access_token.php');
 
@@ -60,32 +61,6 @@ function get_determination_of_mode() {
             exit(1);
     }
     return $mode;
-}
-
-/**
- * Функция берет из базы лиды, по котором нужны звонки
- * @return array: массив лидов
- */
-function get_leads_fromsql() {
-    require_once 'functions/connection.php'; // подключаем скрипт
-    # подключение к  базе amo
-    $link = mysqli_connect($host, $user, $password, $database_amo)
-    or die("Ошибка " . mysqli_error($link));
-
-    $query = "SELECT date, id_lead, count_call from leads";
-
-    $result = mysqli_query($link, $query) or die("Ошибка " . mysqli_error($link));
-
-    if(!$result) {
-        echo "<p>Выполнение запроса прошло не успешно</p>\n\n";
-    }
-
-    $data = [];
-
-    while ($row = $result->fetch_assoc()) {
-        $data[] = $row;
-    }
-    return $data;
 }
 
 /**
