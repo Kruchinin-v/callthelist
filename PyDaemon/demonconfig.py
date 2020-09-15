21807e14c567010f5963a424918e89e092cf7254
Kruchinin-v <vladislav97kr@gmail.com>
2020-09-15 17:02:01 +0300

    Закоментировал строчки, который нужны были для моих тестов. Подготовка к тестам заказчика

--- PyDaemon/demonconfig.py
+++ PyDaemon/demonconfig.py
@@ -58,7 +58,7 @@ class StatCon:
         while (True):
             self.time_alignment()
             now_time = strftime("%H:%M", time.localtime())
-            if now_time in ["16:54"]:
+            if now_time in now_time:
                 print(f"Запуск php скрипта в {now_time}")
                 my_thread = MyThread("/usr/bin/php",
                                      "/var/www/html/amocrm/autoamo/call.php")
